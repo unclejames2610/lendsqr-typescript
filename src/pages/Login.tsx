7b46e89a7b846348d48cf90ec28bbf6b9fa0298f
unclejames2610 <udealaebuka@gmail.com>
2023-04-14 15:45:58 +0100

    log

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,25 +4,35 @@ import heroImg from "../assets/lendsqr-hero.svg";
 
 const Login = () => {
   return (
-    <div>
+    <div className="m-12 w-full h-screen ">
       {/* logo */}
-      <div>
-        <img src={logo} alt="logo" />
+      <div className="">
+        <img src={logo} alt="logo" className="object-cover" />
       </div>
-      <div>
+      <div className="flex flex-col md:flex-row mx-auto justify-center items-center">
         {/* picture */}
         <div>
           <img src={heroImg} alt="hero image" />
         </div>
         {/* form */}
         <div>
-          <h2>Welcome!</h2>
-          <h3>Enter details to login.</h3>
-          <div>
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <h5>FORGOT PASSWORD?</h5>
-            <button>LOG IN</button>
+          <h2 className="text-[40px] leading-[54.64px] text-dark-blue ">
+            Welcome!
+          </h2>
+          <h3 className="text-light-gray text-xl">Enter details to login.</h3>
+          <div className="flex flex-col gap-6 justify-between">
+            <input
+              type="text"
+              placeholder="Email"
+              className="border-2 border-light-gray/[0.15] p-3 text-[14px] rounded-md "
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="border-2 border-light-gray/[0.15] p-3 text-[14px] rounded-md "
+            />
+            <h5 className="text-light-blue">FORGOT PASSWORD?</h5>
+            <button className="bg-light-blue">LOG IN</button>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "dark-blue": "#213F7D",
+      "light-blue": "#39CDCC",
+      "light-gray": "#545F7D",
+    },
   },
   plugins: [],
 };
