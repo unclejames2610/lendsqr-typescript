0fee95bfc49aae60ce8bdce9e5577d6fbc8e8191
unclejames2610 <udealaebuka@gmail.com>
2023-04-27 11:19:28 +0100

    table

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -2,6 +2,7 @@ import React, { FC, useContext, useMemo, useEffect } from "react";
 import { IUserContext, UserContext } from "./UserContext";
 import { Column, useTable } from "react-table";
 import { IUser } from "./interface";
+import { MdOutlineFilterList } from "react-icons/md";
 
 const UsersTable: FC = () => {
   const { users, setUsers } = useContext(UserContext) as IUserContext;
@@ -41,24 +42,32 @@ const UsersTable: FC = () => {
     useTable({ columns, data });
 
   return (
-    <div className="max-w-full mt-8">
-      <table {...getTableProps()}>
-        <thead>
+    <div className="max-w-full mt-12">
+      <table className="text-sm" {...getTableProps()}>
+        <thead className="text-dark-blue ">
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                <th className="relative" {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                  <span className="absolute top-3 right-0">
+                    <MdOutlineFilterList />
+                  </span>
+                </th>
               ))}
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
+        <tbody className="text-light-gray" {...getTableBodyProps()}>
           {rows.map((row) => {
             prepareRow(row);
             return (
-              <tr {...row.getRowProps()}>
+              <tr className="border-b border-light-gray" {...row.getRowProps()}>
                 {row.cells.map((cell) => (
-                  <td {...cell.getCellProps()}> {cell.render("Cell")}</td>
+                  <td className="p-4 " {...cell.getCellProps()}>
+                    {" "}
+                    {cell.render("Cell")}
+                  </td>
                 ))}
               </tr>
             );
