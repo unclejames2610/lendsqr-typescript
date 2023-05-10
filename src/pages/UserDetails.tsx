49a556bbf28925488831d3aac6f9868171d7efd6
unclejames2610 <udealaebuka@gmail.com>
2023-05-10 17:28:07 +0100

    destructuring

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -70,45 +70,58 @@ const UserDetails: FC = () => {
         {/* content */}
         <div className="py-6 px-4 md:px-8 mx-auto mt-6 w-screen">
           <UserDetailsHeader />
-          <div className="flex items-center text-dark-blue  justify-start gap-8 lg:gap-16 mt-8">
-            {/* avatar */}
-            <div
-              className="rounded-full 
+          <div className="border rounded border-dark-blue/[0.06] shadow-[0_3px_5px_20px_rgba(0,0,0,0.04)] ">
+            <div className="flex items-center text-dark-blue  justify-start gap-8 lg:gap-16 mt-8">
+              {/* avatar */}
+              <div
+                className="rounded-full 
             border-none h-[100px] w-[100px]"
-            >
-              {/* <AiOutlineUser className="text-3xl text-dark-blue" /> */}
+              >
+                {/* <AiOutlineUser className="text-3xl text-dark-blue" /> */}
 
-              <img src={avatar} className="rounded-full object-fill" />
-            </div>
+                <img src={avatar} className="rounded-full object-fill" />
+              </div>
 
-            {/* name */}
-            <div>
-              <p className="text-[22px] leading-[26px] font-[500]">
-                {userName}
-              </p>
-              <p className="text-light-gray text-sm leading-4 font-normal">
-                {accountNumber}
-              </p>
+              {/* name */}
+              <div>
+                <p className="text-[22px] leading-[26px] font-[500]">
+                  {userName}
+                </p>
+                <p className="text-light-gray text-sm leading-4 font-normal">
+                  {accountNumber}
+                </p>
+              </div>
+              {/* user tier */}
+              <div className="border-x border-light-gray/20 p-8">
+                <p className="text-light-gray text-sm leading-4 font-normal">
+                  User's Tier
+                </p>
+                <p className="flex text-custom-gold">
+                  <AiFillStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                </p>
+              </div>
+              {/* balance */}
+              <div>
+                <p className="text-[22px] leading-[26px] font-[500]">
+                  N{accountBalance}
+                </p>
+                <p className="text-xs leading-[14px]">
+                  {accountNumber}/Providus Bank
+                </p>
+              </div>
             </div>
-            {/* user tier */}
-            <div className="border-x border-light-gray/20 p-8">
-              <p className="text-light-gray text-sm leading-4 font-normal">
-                User's Tier
+            {/* Tabs */}
+            <div className="flex justify-between mt-8 leading-[19px]">
+              <p className="text-center px-2 border-b-2 text-light-blue border-light-blue">
+                General Details
               </p>
-              <p className="flex text-custom-gold">
-                <AiFillStar />
-                <AiOutlineStar />
-                <AiOutlineStar />
-              </p>
-            </div>
-            {/* balance */}
-            <div>
-              <p className="text-[22px] leading-[26px] font-[500]">
-                N{accountBalance}
-              </p>
-              <p className="text-xs leading-[14px]">
-                {accountNumber}/Providus Bank
-              </p>
+              <p className="text-center px-2">Documents</p>
+              <p className="text-center px-2">Bank Details</p>
+              <p className="text-center px-2">Loans</p>
+              <p className="text-center px-2">Savings</p>
+              <p className="text-center px-2">App and System</p>
             </div>
           </div>
         </div>
